f655378bb0b2e0d2f644b0ea5ced2f556d76701b
Anas Abubakar <anasabubakar2015@gmail.com>
2022-08-31 15:32:35 +0100

    Add action and reducer for filter

--- src/redux/crypto/crypto.js
+++ src/redux/crypto/crypto.js
@@ -1,11 +1,24 @@
 const FETCH_CRYPTO = 'cryptoStat/crypto/FETCH_CRYPTO';
-
+const SEARCH_CRYPTO = 'cryptoStat/crypto/SEARCH_CRYPTO';
+const CLEAR_SEARCH = 'cryptoStat/crypto/CLEAR_SEARCH';
 const initialState = [];
 
 const cryptoReducer = (state = initialState, action) => {
   switch (action.type) {
     case FETCH_CRYPTO:
       return action.crypto;
+    case SEARCH_CRYPTO:
+      return {
+        ...state,
+        search: state.crypto.filter((coin) => coin.id === action.payload
+        || coin.symbol.toLowerCase() === action.payload
+        || coin.name.toLowerCase() === action.payload),
+      };
+    case CLEAR_SEARCH:
+      return {
+        ...state,
+        search: [],
+      };
     default:
       return state;
   }
@@ -16,16 +29,14 @@ export const fetchCrypto = (crypto) => ({
   crypto,
 });
 
-// export const fetchCryptoApi = () => async (dispatch) => {
-//   const result = await fetch('https://api.coincap.io/v2/assets#');
-//   const json = await result.json();
-//   const crypto = json.map((coins) => ({
-//     id: coins.data.id,
-//     name: coins.data.name,
-//     price: coins.data.price,
-//   }));
-//   dispatch(fetchCrypto(crypto));
-// };
+export const searchCoin = (coin) => ({
+  type: SEARCH_CRYPTO,
+  payload: coin.toLowerCase(),
+});
+
+export const reset = () => ({
+  type: CLEAR_SEARCH,
+});
 
 export const fetchCryptoApi = () => (dispatch) => fetch('https://api.coincap.io/v2/assets#')
   .then((res) => res.json())
